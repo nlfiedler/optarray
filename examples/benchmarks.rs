eb556376c30ffa0ddb0bc3e1b087da8268f5e51a
Nathan Fiedler <nathanfiedler@fastmail.fm>
2025-08-24 21:11:27 -0700

    test: add more benchmarks

--- examples/benchmarks.rs
+++ examples/benchmarks.rs
@@ -2,6 +2,7 @@
 // Copyright (c) 2025 Nathan Fiedler
 //
 use optarray::OptimalArray;
+use rand::{Rng, SeedableRng, rngs::SmallRng};
 use std::time::Instant;
 
 //
@@ -9,29 +10,67 @@ use std::time::Instant;
 // time than a vector, however in practice that is not the case.
 //
 
-fn create_optarray(size: u64) {
+fn benchmark_optarray(size: usize) {
     let start = Instant::now();
-    let mut coll: OptimalArray<u64> = OptimalArray::new();
+    let mut coll: OptimalArray<usize> = OptimalArray::new();
     for value in 0..size {
         coll.push(value);
     }
     let duration = start.elapsed();
-    println!("optarray: {:?}", duration);
+    println!("optarray create: {:?}", duration);
+
+    // test random access `size` times; use SmallRng to avoid dominating the
+    // running time with random number generation
+    let mut rng = SmallRng::seed_from_u64(0);
+    let start = Instant::now();
+    for _ in 0..size {
+        let index = rng.random_range(0..size);
+        assert_eq!(coll[index], index);
+    }
+    let duration = start.elapsed();
+    println!("optarray random: {:?}", duration);
+
+    // test sequenced access for entire collection
+    let start = Instant::now();
+    for (index, value) in coll.iter().enumerate() {
+        assert_eq!(*value, index);
+    }
+    let duration = start.elapsed();
+    println!("optarray ordered: {:?}", duration);
 }
 
-fn create_vector(size: u64) {
+fn benchmark_vector(size: usize) {
     let start = Instant::now();
-    let mut coll: Vec<u64> = Vec::new();
+    let mut coll: Vec<usize> = Vec::new();
     for value in 0..size {
         coll.push(value);
     }
     let duration = start.elapsed();
-    println!("vector: {:?}", duration);
+    println!("vector create: {:?}", duration);
+
+    // test random access `size` times; use SmallRng to avoid dominating the
+    // running time with random number generation
+    let mut rng = SmallRng::seed_from_u64(0);
+    let start = Instant::now();
+    for _ in 0..size {
+        let index = rng.random_range(0..size);
+        assert_eq!(coll[index], index);
+    }
+    let duration = start.elapsed();
+    println!("vector random: {:?}", duration);
+
+    // test sequenced access for entire collection
+    let start = Instant::now();
+    for (index, value) in coll.iter().enumerate() {
+        assert_eq!(*value, index);
+    }
+    let duration = start.elapsed();
+    println!("vector ordered: {:?}", duration);
 }
 
 fn main() {
     println!("creating OptimalArray...");
-    create_optarray(100_000_000);
+    benchmark_optarray(100_000_000);
     println!("creating Vec...");
-    create_vector(100_000_000);
+    benchmark_vector(100_000_000);
 }
